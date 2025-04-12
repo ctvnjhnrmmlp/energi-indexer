cf5102676a9fe6f08ed182ebfd28514222dc5729
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2025-04-12 20:52:28 +0800

    fix: added new logic and structure for indexer service

--- indexer/src/indexer.core.ts
+++ indexer/src/indexer.core.ts
@@ -4,50 +4,53 @@ import Prisma from '../database/database';
 
 dotenv.config();
 
-export class IndexerInstance {
-  public client;
-
-  constructor() {
-    this.client = createPublicClient({
-      transport: http('https://nodeapi.energi.network'),
-      chain: {
-        id: 39797,
-        name: 'Energi',
-        nativeCurrency: { name: 'Energi', symbol: 'NRG', decimals: 18 },
-        rpcUrls: {
-          default: {
-            http: ['https://nodeapi.energi.network'],
-          },
-        },
+const client = createPublicClient({
+  transport: http('https://nodeapi.energi.network'),
+  chain: {
+    id: 39797,
+    name: 'Energi',
+    nativeCurrency: { name: 'Energi', symbol: 'NRG', decimals: 18 },
+    rpcUrls: {
+      default: {
+        http: ['https://nodeapi.energi.network'],
       },
-    });
-  }
+    },
+  },
+});
 
-  public async scanBlocks({ from, to }: { from: number; to?: number }) {
-    const latest = to ?? this.client.getBlockNumber();
+async function scanBlock(blockNumber: bigint) {
+  const block = await client.getBlock({
+    blockNumber,
+    includeTransactions: true,
+  });
 
-    for (let i = from; i <= Number(latest); i++) {
-      const block = await this.client.getBlock({
-        blockNumber: BigInt(i),
-      });
-      const txs = block.transactions;
+  await Prisma.block.upsert({
+    where: { number: Number(block.number) },
+    update: {},
+    create: {
+      number: Number(block.number),
+      hash: block.hash,
+      txs: {
+        create: block.transactions.map((tx) => ({
+          hash: tx.hash,
+          from: tx.from,
+          to: tx.to ?? '',
+          nonce: Number(tx.nonce),
+          amount: Number(tx.value),
+          blockNumber: Number(block.number),
+        })),
+      },
+    },
+  });
+}
 
-      await Prisma.block.create({
-        data: {
-          number: i,
-          hash: block.hash,
-          txs: {
-            create: txs.map((hash) => ({
-              hash,
-              from: '',
-              to: '',
-              nonce: 0,
-              amount: 0,
-              blockNumber: i,
-            })),
-          },
-        },
-      });
-    }
+export async function runIndexer(from?: number, to?: number) {
+  const latestBlock = Number(await client.getBlockNumber());
+  const start = from ?? 0;
+  const end = to ?? latestBlock;
+
+  for (let i = start; i <= end; i++) {
+    console.log(`Indexing block ${i}`);
+    await scanBlock(BigInt(i));
   }
 }
